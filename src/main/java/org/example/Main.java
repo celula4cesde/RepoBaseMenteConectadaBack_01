69c3f5f42325e2e1903a8e476db72fd192a61656
MateoPinedaBel <MateoPinedaBel@Outlook.com>
2025-07-22 09:48:12 -0500

    Refactor: Clase separada.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,6 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        int contadorVisitas = 0;
 
-        // Simulación de 10 visitas al perfil
-        for (int i = 0; i < 10; i++) {
-            contadorVisitas++;
-            System.out.println("Visita #" + (i + 1) + " registrada.");
-        }
-
-        // Mostrar total al final
-        System.out.println("Total de visitas al perfil: " + contadorVisitas);
     }
 }
